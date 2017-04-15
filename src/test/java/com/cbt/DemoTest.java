16eb548b52ff73e04568d2d1242a1611ec7f578a
Madina7777 <akabirovamadina@gmail.com>
2017-04-15 15:16:30 -0400

    this is Madinas commit

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -4,5 +4,14 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("hello again");
+		
+		
+		for(int num=0 ; num<=100; num++){
+			if(num%2==0){
+				System.out.println(num);
+			}
+		}
+		
+		
 	}
 }
