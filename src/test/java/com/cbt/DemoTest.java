71ef710562b5893242a28aebe825eeca28f5981f
Mashenka686 <masha.brown.qa@gmail.com>
2017-04-15 15:19:04 -0400

    added another useless print statement

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -4,5 +4,6 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("hello again");
+		System.out.println("Hi, Hi, Hi!");
 	}
 }
