79573b47ca591dea55cea4156decd3b9260d2b1d
aebishkek <aebishkek@gmail.com>
2017-04-15 18:34:51 -0400

    Hello from MD

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -5,6 +5,7 @@ public class DemoTest {
 		System.out.println("Hello world");
 		System.out.println("hello again");
 		System.out.println("Hello from LA,CA");
+		System.out.println("Hello from MD");
 		
 		
 		
