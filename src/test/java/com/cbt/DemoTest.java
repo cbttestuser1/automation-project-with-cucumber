3057f5fb09b27a457b605532b5fa0dd70970d479
Djkarimov7 <jama84@list.ru>
2017-04-15 15:28:29 -0400

    Djamshed's commit

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -4,6 +4,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("hello again");
+		System.out.println("Hello from LA,CA");
 		
 		
 		for(int num=0 ; num<=100; num++){
