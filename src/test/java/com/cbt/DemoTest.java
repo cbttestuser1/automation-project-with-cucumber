6b2261e5d0cf1acdc72c71cfadfc01c9294691a9
zaferny <zaferny@gmail.com>
2017-05-14 19:16:16 -0400

    qwdefrgth

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -3,11 +3,10 @@ package com.cbt;
 public class DemoTest {
 	public static void main(String[] args) {
        
-
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello  World");
-		System.out.println("Hello Chicagooo");
+		System.out.println("Hello Chicago");
 		System.out.println("What's up!");
 	}
 }
